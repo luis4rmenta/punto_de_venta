77047dd48a238b71b1976ee6c8f47265c9fd8c87
beku86 <luigiangelxd@gmail.com>
2020-11-29 23:39:24 -0600

    Navbar implementation

--- client/canningCart/src/app/components/navbar/navbar.component.ts
+++ client/canningCart/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -6,11 +6,20 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  constructor(public authService: AuthService) { }
+export class NavbarComponent implements OnInit{
+  roll: string;
+
+  constructor(public authService: AuthService) { }  
 
   ngOnInit(): void {
-
+    this.authService.getUserType().subscribe(res => this.roll = res.roll, err => console.log);
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    // this.authService.getUserType().subscribe(res => this.roll = res.roll, err => console.log);
+    
   }
 
 
