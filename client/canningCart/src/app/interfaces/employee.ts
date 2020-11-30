d4ca4e37d5bccdf7d8e3b9a2973835ed7a67b0f6
beku86 <luigiangelxd@gmail.com>
2020-11-29 23:37:31 -0600

    New interfaces

---
+++ client/canningCart/src/app/interfaces/employee.ts
@@ -0,0 +1,6 @@
+export interface Employee {
+    empleado_id?: number,
+    persona_id?:  number,
+    tipo_empleado_id: number | string,
+    fecha: string
+}
---
+++ client/canningCart/src/app/interfaces/person.ts
@@ -0,0 +1,12 @@
+export interface Person {
+    person_id?: number,
+    curp: string,
+    nombre: string,
+    primer_apellido: string,
+    segundo_apellido: string
+    direccion: string,
+    telefono: string,
+    email: string,
+    genero: string,
+    fecha_de_nacimiento: string
+}
